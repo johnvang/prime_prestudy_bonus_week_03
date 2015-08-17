21139d565f93cf0e04a99e83e687ba4f6e945ca4
johnvang <johnvang@Johns-MacBook-Pro.local>
2015-08-17 00:15:56 -0500

    first commit

---
+++ wk3_bonus.js
@@ -0,0 +1,16 @@
+function functionA(a) {
+	x = a * 10;
+	return x;
+}
+
+function functionB(b) {
+	x = b / 28;
+	return x;
+}
+
+function functionC(c) {
+	x = c + 82;
+	return x;
+}
+
+functionA(functionB(FunctionC(2)));
